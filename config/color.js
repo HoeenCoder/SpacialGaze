8ef912101a798c4a6c0bd5744eb6c8e04189a44b
Kraken Mare <RagiingAlways@gmail.com>
2016-12-05 17:51:46 -0500

    Update color.js

--- config/color.js
+++ config/color.js
@@ -39,14 +39,14 @@ function MD5(e) {
 		return t
 	}
 	var u = [],
-		a, f, l, c, h, p, d, v, e = function(e) {
+		a, f, l, c, h, p, d, v, e = function (e) {
 			for (var e = e.replace(/\r\n/g, "\n"), t = "", n = 0; n < e.length; n++) {
 				var r = e.charCodeAt(n);
 				r < 128 ? t += String.fromCharCode(r) : (r > 127 && r < 2048 ? t += String.fromCharCode(r >> 6 | 192) : (t += String.fromCharCode(r >> 12 | 224), t += String.fromCharCode(r >> 6 & 63 | 128)), t += String.fromCharCode(r & 63 | 128));
 			}
 			return t;
 		}(e),
-		u = function(e) {
+		u = function (e) {
 			var t, n = e.length;
 			t = n + 8;
 			for (var r = ((t - t % 64) / 64 + 1) * 16, i = Array(r - 1), s = 0, o = 0; o < n;) t = (o - o % 4) / 4, s = o % 4 * 8, i[t] |= e.charCodeAt(o) << s, o++;
@@ -140,12 +140,37 @@ function hashColor(name) {
 
 	var R1, G1, B1;
 	switch (Math.floor(H / 60)) {
-		case 1: R1 = X; G1 = C; B1 = 0; break;
-		case 2: R1 = 0; G1 = C; B1 = X; break;
-		case 3: R1 = 0; G1 = X; B1 = C; break;
-		case 4: R1 = X; G1 = 0; B1 = C; break;
-		case 5: R1 = C; G1 = 0; B1 = X; break;
-		case 0: default: R1 = C; G1 = X; B1 = 0; break;
+	case 1:
+		R1 = X;
+		G1 = C;
+		B1 = 0;
+		break;
+	case 2:
+		R1 = 0;
+		G1 = C;
+		B1 = X;
+		break;
+	case 3:
+		R1 = 0;
+		G1 = X;
+		B1 = C;
+		break;
+	case 4:
+		R1 = X;
+		G1 = 0;
+		B1 = C;
+		break;
+	case 5:
+		R1 = C;
+		G1 = 0;
+		B1 = X;
+		break;
+	case 0:
+	default:
+		R1 = C;
+		G1 = X;
+		B1 = 0;
+		break;
 	}
 	var lum = (R1 + m) * 0.2126 + (G1 + m) * 0.7152 + (B1 + m) * 0.0722; // 0.05 (dark blue) to 0.93 (yellow)
 	var HLmod = (lum - 0.5) * -100; // -43 (yellow) to 45 (dark blue)
