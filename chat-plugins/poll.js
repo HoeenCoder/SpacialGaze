d226e737f543106659895664cd12ff62e32fefa3
Kraken Mare <RagiingAlways@gmail.com>
2016-12-05 17:46:10 -0500

    Update poll.js

--- chat-plugins/poll.js
+++ chat-plugins/poll.js
@@ -1,8 +1,7 @@
 /*
-* Poll chat plugin
-* By bumbadadabum and Zarel.
-*/
-
+ * Poll chat plugin
+ * By bumbadadabum and Zarel.
+ */
 'use strict';
 
 class Poll {
@@ -23,7 +22,10 @@ class Poll {
 
 		this.options = new Map();
 		for (let i = 0; i < options.length; i++) {
-			this.options.set(i + 1, {name: options[i], votes: 0});
+			this.options.set(i + 1, {
+				name: options[i],
+				votes: 0
+			});
 		}
 	}
 
@@ -180,158 +182,161 @@ class Poll {
 exports.Poll = Poll;
 
 exports.commands = {
-	poll: {
-		htmlcreate: 'new',
-		create: 'new',
-		new: function (target, room, user, connection, cmd, message) {
-			if (!target) return this.parse('/help poll new');
-			if (target.length > 1024) return this.errorReply("Poll too long.");
+		poll: {
+			htmlcreate: 'new',
+			create: 'new',
+			new: function (target, room, user, connection, cmd, message) {
+				if (!target) return this.parse('/help poll new');
+				if (target.length > 1024) return this.errorReply("Poll too long.");
 
-			const supportHTML = cmd === 'htmlcreate';
-			let separator = '';
-			if (target.includes('\n')) {
-				separator = '\n';
-			} else if (target.includes('|')) {
-				separator = '|';
-			} else if (target.includes(',')) {
-				separator = ',';
-			} else {
-				return this.errorReply("Not enough arguments for /poll new.");
-			}
-
-			let params = target.split(separator).map(param => param.trim());
-
-			if (!this.can('minigame', null, room)) return false;
-			if (supportHTML && !this.can('declare', null, room)) return false;
-			if (!this.canTalk()) return;
-			if (room.poll) return this.errorReply("There is already a poll in progress in this room.");
-			if (params.length < 3) return this.errorReply("Not enough arguments for /poll new.");
-
-			if (supportHTML) params = params.map(parameter => this.canHTML(parameter));
-			if (params.some(parameter => !parameter)) return;
-
-			const options = params.splice(1);
-			if (options.length > 8) {
-				return this.errorReply("Too many options for poll (maximum is 8).");
-			}
-
-			room.poll = new Poll(room, {source: params[0], supportHTML: supportHTML}, options);
-			room.poll.display();
-
-			this.logEntry("" + user.name + " used " + message);
-			return this.privateModCommand("(A poll was started by " + user.name + ".)");
-		},
-		newhelp: ["/poll create [question], [option1], [option2], [...] - Creates a poll. Requires: % @ * # & ~"],
-
-		vote: function (target, room, user) {
-			if (!room.poll) return this.errorReply("There is no poll running in this room.");
-			if (!target) return this.parse('/help poll vote');
-
-			if (target === 'blank') {
-				room.poll.blankvote(user);
-				return;
-			}
-
-			let parsed = parseInt(target);
-			if (isNaN(parsed)) return this.errorReply("To vote, specify the number of the option.");
-
-			if (!room.poll.options.has(parsed)) return this.sendReply("Option not in poll.");
-
-			room.poll.vote(user, parsed);
-		},
-		votehelp: ["/poll vote [number] - Votes for option [number]."],
-
-		timer: function (target, room, user) {
-			if (!room.poll) return this.errorReply("There is no poll running in this room.");
-
-			if (target) {
-				if (!this.can('minigame', null, room)) return false;
-				if (target === 'clear') {
-					if (!room.poll.timeout) return this.errorReply("There is no timer to clear.");
-					clearTimeout(room.poll.timeout);
-					room.poll.timeout = null;
-					room.poll.timeoutMins = 0;
-					return this.add("The poll timer was turned off.");
-				}
-				let timeout = parseFloat(target);
-				if (isNaN(timeout) || timeout <= 0 || timeout > 0x7FFFFFFF) return this.errorReply("Invalid time given.");
-				if (room.poll.timeout) clearTimeout(room.poll.timeout);
-				room.poll.timeoutMins = timeout;
-				room.poll.timeout = setTimeout(() => {
-					room.poll.end();
-					delete room.poll;
-				}, (timeout * 60000));
-				room.add("The poll timer was turned on: the poll will end in " + timeout + " minute(s).");
-				return this.privateModCommand("(The poll timer was set to " + timeout + " minute(s) by " + user.name + ".)");
-			} else {
-				if (!this.runBroadcast()) return;
-
-				if (room.poll.timeout) {
-					return this.sendReply("The poll timer is on and will end in " + room.poll.timeoutMins + " minute(s).");
+				const supportHTML = cmd === 'htmlcreate';
+				let separator = '';
+				if (target.includes('\n')) {
+					separator = '\n';
+				} else if (target.includes('|')) {
+					separator = '|';
+				} else if (target.includes(',')) {
+					separator = ',';
 				} else {
-					return this.sendReply("The poll timer is off.");
+					return this.errorReply("Not enough arguments for /poll new.");
 				}
-			}
 
-		},
-		timerhelp: ["/poll timer [minutes] - Sets the poll to automatically end after [minutes] minutes. Requires: % @ * # & ~", "/poll timer clear - Clears the poll's timer. Requires: % @ * # & ~"],
+				let params = target.split(separator).map(param => param.trim());
 
-		results: function (target, room, user) {
-			if (!room.poll) return this.errorReply("There is no poll running in this room.");
+				if (!this.can('minigame', null, room)) return false;
+				if (supportHTML && !this.can('declare', null, room)) return false;
+				if (!this.canTalk()) return;
+				if (room.poll) return this.errorReply("There is already a poll in progress in this room.");
+				if (params.length < 3) return this.errorReply("Not enough arguments for /poll new.");
 
-			return room.poll.blankvote(user);
-		},
-		resultshelp: ["/poll results - Shows the results of the poll without voting. NOTE: you can't go back and vote after using this."],
+				if (supportHTML) params = params.map(parameter => this.canHTML(parameter));
+				if (params.some(parameter => !parameter)) return;
 
-		close: 'end',
-		stop: 'end',
-		end: function (target, room, user) {
-			if (!this.can('minigame', null, room)) return false;
-			if (!this.canTalk()) return;
-			if (!room.poll) return this.errorReply("There is no poll running in this room.");
-			if (room.poll.timeout) clearTimeout(room.poll.timeout);
+				const options = params.splice(1);
+				if (options.length > 8) {
+					return this.errorReply("Too many options for poll (maximum is 8).");
+				}
 
-			room.poll.end();
-			delete room.poll;
-			return this.privateModCommand("(The poll was ended by " + user.name + ".)");
-		},
-		endhelp: ["/poll end - Ends a poll and displays the results. Requires: % @ * # & ~"],
-
-		show: 'display',
-		display: function (target, room, user, connection) {
-			if (!room.poll) return this.errorReply("There is no poll running in this room.");
-			if (!this.runBroadcast()) return;
-			room.update();
-
-			if (this.broadcasting) {
+				room.poll = new Poll(room, {
+					source: params[0],
+					supportHTML: supportHTML
+				}, options);
 				room.poll.display();
-			} else {
-				room.poll.displayTo(user, connection);
-			}
-		},
-		displayhelp: ["/poll display - Displays the poll"],
 
-		'': function (target, room, user) {
-			this.parse('/help poll');
+				this.logEntry("" + user.name + " used " + message);
+				return this.privateModCommand("(A poll was started by " + user.name + ".)");
+			},
+			newhelp: ["/poll create [question], [option1], [option2], [...] - Creates a poll. Requires: % @ * # & ~"],
+
+			vote: function (target, room, user) {
+				if (!room.poll) return this.errorReply("There is no poll running in this room.");
+				if (!target) return this.parse('/help poll vote');
+
+				if (target === 'blank') {
+					room.poll.blankvote(user);
+					return;
+				}
+
+				let parsed = parseInt(target);
+				if (isNaN(parsed)) return this.errorReply("To vote, specify the number of the option.");
+
+				if (!room.poll.options.has(parsed)) return this.sendReply("Option not in poll.");
+
+				room.poll.vote(user, parsed);
+			},
+			votehelp: ["/poll vote [number] - Votes for option [number]."],
+
+			timer: function (target, room, user) {
+				if (!room.poll) return this.errorReply("There is no poll running in this room.");
+
+				if (target) {
+					if (!this.can('minigame', null, room)) return false;
+					if (target === 'clear') {
+						if (!room.poll.timeout) return this.errorReply("There is no timer to clear.");
+						clearTimeout(room.poll.timeout);
+						room.poll.timeout = null;
+						room.poll.timeoutMins = 0;
+						return this.add("The poll timer was turned off.");
+					}
+					let timeout = parseFloat(target);
+					if (isNaN(timeout) || timeout <= 0 || timeout > 0x7FFFFFFF) return this.errorReply("Invalid time given.");
+					if (room.poll.timeout) clearTimeout(room.poll.timeout);
+					room.poll.timeoutMins = timeout;
+					room.poll.timeout = setTimeout(() => {
+						room.poll.end();
+						delete room.poll;
+					}, (timeout * 60000));
+					room.add("The poll timer was turned on: the poll will end in " + timeout + " minute(s).");
+					return this.privateModCommand("(The poll timer was set to " + timeout + " minute(s) by " + user.name + ".)");
+				} else {
+					if (!this.runBroadcast()) return;
+
+					if (room.poll.timeout) {
+						return this.sendReply("The poll timer is on and will end in " + room.poll.timeoutMins + " minute(s).");
+					} else {
+						return this.sendReply("The poll timer is off.");
+					}
+				}
+
+			},
+			timerhelp: ["/poll timer [minutes] - Sets the poll to automatically end after [minutes] minutes. Requires: % @ * # & ~", "/poll timer clear - Clears the poll's timer. Requires: % @ * # & ~"],
+
+			results: function (target, room, user) {
+				if (!room.poll) return this.errorReply("There is no poll running in this room.");
+
+				return room.poll.blankvote(user);
+			},
+			resultshelp: ["/poll results - Shows the results of the poll without voting. NOTE: you can't go back and vote after using this."],
+
+			close: 'end',
+			stop: 'end',
+			end: function (target, room, user) {
+				if (!this.can('minigame', null, room)) return false;
+				if (!this.canTalk()) return;
+				if (!room.poll) return this.errorReply("There is no poll running in this room.");
+				if (room.poll.timeout) clearTimeout(room.poll.timeout);
+
+				room.poll.end();
+				delete room.poll;
+				return this.privateModCommand("(The poll was ended by " + user.name + ".)");
+			},
+			endhelp: ["/poll end - Ends a poll and displays the results. Requires: % @ * # & ~"],
+
+			show: 'display',
+			display: function (target, room, user, connection) {
+				if (!room.poll) return this.errorReply("There is no poll running in this room.");
+				if (!this.runBroadcast()) return;
+				room.update();
+
+				if (this.broadcasting) {
+					room.poll.display();
+				} else {
+					room.poll.displayTo(user, connection);
+				}
+			},
+			displayhelp: ["/poll display - Displays the poll"],
+
+			'': function (target, room, user) {
+				this.parse('/help poll');
+			},
 		},
-	},
-	pollhelp: [
-		"/poll allows rooms to run their own polls. These polls are limited to one poll at a time per room.",
-		"Accepts the following commands:",
-		"/poll create [question], [option1], [option2], [...] - Creates a poll. Requires: % @ * # & ~",
-		"/poll htmlcreate [question], [option1], [option2], [...] - Creates a poll, with HTML allowed in the question and options. Requires: # & ~",
-		"/poll vote [number] - Votes for option [number].",
-		"/poll timer [minutes] - Sets the poll to automatically end after [minutes]. Requires: % @ * # & ~",
-		"/poll results - Shows the results of the poll without voting. NOTE: you can't go back and vote after using this.",
-		"/poll display - Displays the poll",
-		"/poll end - Ends a poll and displays the results. Requires: % @ * # & ~",
-	],
-	tpoll: 'tierpoll',
-	tierpoll: function (target, room, user, connection, cmd, message) {
-		if (!this.can('minigame', null, room)) return false;
-		if (room.poll) return this.errorReply("There is already a poll in progress in this room.");
-		let options = [];
-		for (let key in Tools.data.Formats) {
+		pollhelp: [
+			"/poll allows rooms to run their own polls. These polls are limited to one poll at a time per room.",
+			"Accepts the following commands:",
+			"/poll create [question], [option1], [option2], [...] - Creates a poll. Requires: % @ * # & ~",
+			"/poll htmlcreate [question], [option1], [option2], [...] - Creates a poll, with HTML allowed in the question and options. Requires: # & ~",
+			"/poll vote [number] - Votes for option [number].",
+			"/poll timer [minutes] - Sets the poll to automatically end after [minutes]. Requires: % @ * # & ~",
+			"/poll results - Shows the results of the poll without voting. NOTE: you can't go back and vote after using this.",
+			"/poll display - Displays the poll",
+			"/poll end - Ends a poll and displays the results. Requires: % @ * # & ~",
+		],
+		tpoll: 'tierpoll',
+		tierpoll: function (target, room, user, connection, cmd, message) {
+			if (!this.can('minigame', null, room)) return false;
+			if (room.poll) return this.errorReply("There is already a poll in progress in this room.");
+			let options = [];
+			for (let key in Tools.data.Formats) {
 				if (!Tools.data.Formats[key].mod) continue;
 				if (!Tools.data.Formats[key].searchShow) continue;
 				if (toId(target) !== 'all') {
@@ -339,15 +344,18 @@ exports.commands = {
 					if (commonMods.indexOf(Tools.data.Formats[key].mod) === -1) continue;
 				}
 				options.push(Tools.data.Formats[key].name);
-		}
-		room.poll = new Poll(room, {source: 'What should the next tournament tier be?', supportHTML: false}, options);
-		room.poll.display();
-		this.logEntry("" + user.name + " used " + message);
-		return this.privateModCommand("(A tier poll was started by " + user.name + ".)");
+			}
+			room.poll = new Poll(room, {
+				source: 'What should the next tournament tier be?',
+				supportHTML: false
+			}, options);
+			room.poll.display();
+			this.logEntry("" + user.name + " used " + message);
+			return this.privateModCommand("(A tier poll was started by " + user.name + ".)");
 		},
 		tierpollhelp: ["/tierpoll - (all) Creates a poll with all the common formats as options. All all to use all formats Requires: % @ * # & ~"],
-},
+	},
 
-process.nextTick(() => {
-	Chat.multiLinePattern.register('/poll (new|create|htmlcreate) ');
-});
+	process.nextTick(() => {
+		Chat.multiLinePattern.register('/poll (new|create|htmlcreate) ');
+	});
