d7224e399a3ec510ece9525533fe7757f90ff652
Kraken Mare <RagiingAlways@gmail.com>
2016-12-05 19:45:37 -0500

    Update scripts.js

--- mods/sgssb/scripts.js
+++ mods/sgssb/scripts.js
@@ -14,13 +14,13 @@ exports.BattleScripts = {
 				item: 'Leftovers',
 				gender: 'M',
 				moves: [
-					['Hydro Pump', 'Scald'][variant], 'Ice Beam', 'Giga Drain'
+					['Hydro Pump', 'Scald'][variant], 'Ice Beam', 'Giga Drain',
 				],
 				signatureMove: 'Scripting',
 				evs: {
 					spa: 252,
 					spe: 252,
-					hp: 4
+					hp: 4,
 				},
 				nature: 'Modest',
 			},
@@ -30,13 +30,13 @@ exports.BattleScripts = {
 				item: 'Eviolite',
 				gender: 'F',
 				moves: [
-					['Calm Mind', 'Cosmic Power'][variant], 'Soft-Boiled', 'Stored Power'
+					['Calm Mind', 'Cosmic Power'][variant], 'Soft-Boiled', 'Stored Power',
 				],
 				signatureMove: 'Mystic Mirage',
 				evs: {
 					hp: 252,
 					def: 252,
-					spd: 4
+					spd: 4,
 				},
 				nature: 'Bold',
 			},
@@ -46,12 +46,12 @@ exports.BattleScripts = {
 				species: 'Genesect',
 				ability: 'Download',
 				item: 'Choice Scarf',
-				moves: ['U-turn', 'Ice Beam', 'Explosion'],
+				moves: ['U-turn', 'Ice Beam', 'Explosion',],
 				signatureMove: 'Crystallized Ukaku',
 				evs: {
 					atk: 252,
 					spe: 252,
-					spa: 4
+					spa: 4,
 				},
 				nature: 'Hasty',
 			},
@@ -60,12 +60,12 @@ exports.BattleScripts = {
 				ability: 'Multiscale',
 				item: 'Weakness Policy',
 				gender: 'M',
-				moves: ['Fire Punch', 'Dragon Claw', 'Waterfall'],
+				moves: ['Fire Punch', 'Dragon Claw', 'Waterfall',],
 				signatureMove: 'Ancient Orb',
 				evs: {
 					atk: 252,
 					spe: 252,
-					hp: 4
+					hp: 4,
 				},
 				nature: 'Adamant',
 			},
@@ -75,12 +75,12 @@ exports.BattleScripts = {
 				shiny: true,
 				item: 'Gardevoirite',
 				gender: 'F',
-				moves: ['Moonblast', 'Calm Mind', 'Psychic'],
+				moves: ['Moonblast', 'Calm Mind', 'Psychic',],
 				signatureMove: 'Mega Rage',
 				evs: {
 					hp: 248,
 					spa: 252,
-					def: 8
+					def: 8,
 				},
 				nature: 'Modest',
 			},
@@ -90,12 +90,12 @@ exports.BattleScripts = {
 				ability: 'Levitate',
 				item: 'Leftovers',
 				gender: (variant === 1) ? 'M' : 'F',
-				moves: ['Gyro Ball', 'Confuse Ray', 'Toxic', 'Earthquake'],
+				moves: ['Gyro Ball', 'Confuse Ray', 'Toxic', 'Earthquake',],
 				signatureMove: 'Blast Furnace',
 				evs: {
 					hp: 252,
 					def: 168,
-					spd: 88
+					spd: 88,
 				},
 				nature: 'Sassy',
 			},
@@ -104,11 +104,11 @@ exports.BattleScripts = {
 				ability: 'Bulletproof',
 				item: 'Safety Goggles',
 				gender: 'M',
-				moves: ['Assist', 'Beat Up', 'Sucker Punch', 'Heavy Slam'],
+				moves: ['Assist', 'Beat Up', 'Sucker Punch', 'Heavy Slam',],
 				signatureMove: 'Lightshot Giga-Lance',
 				evs: {
 					hp: 252,
-					atk: 252
+					atk: 252,
 				},
 				nature: 'Adamant',
 			},
@@ -117,12 +117,12 @@ exports.BattleScripts = {
 				ability: 'Serene Grace',
 				item: 'Kings Rock',
 				gender: 'M',
-				moves: ['Roost', 'Coil', 'Rock Slide', ['Glare', 'Body Slam'][variant]],
+				moves: ['Roost', 'Coil', 'Rock Slide', ['Glare', 'Body Slam'][variant],],
 				signatureMove: 'Getting Trolled',
 				evs: {
 					hp: 252,
 					atk: 252,
-					def: 4
+					def: 4,
 				},
 				nature: 'Naughty',
 			},
@@ -131,12 +131,12 @@ exports.BattleScripts = {
 				ability: 'Limber',
 				item: 'Lopunnite',
 				gender: 'F',
-				moves: ['Fake out', 'Return', 'High Jump Kick'],
+				moves: ['Fake out', 'Return', 'High Jump Kick',],
 				signatureMove: 'Charm Up',
 				evs: {
 					atk: 252,
 					spe: 252,
-					def: 4
+					def: 4,
 				},
 				nature: 'Jolly',
 			},
@@ -145,13 +145,13 @@ exports.BattleScripts = {
 				ability: 'Levitate',
 				item: 'Choice Specs',
 				moves: [
-					['Volt Switch', 'Thunderbolt'][this.random(2)], 'Hidden Power Ice', 'Aura Sphere'
+					['Volt Switch', 'Thunderbolt'][this.random(2)], 'Hidden Power Ice', 'Aura Sphere',
 				],
 				signatureMove: 'Mechanical Dysfunction',
 				evs: {
 					spa: 252,
 					spe: 252,
-					hp: 4
+					hp: 4,
 				},
 				nature: 'Modest',
 			},
@@ -162,12 +162,12 @@ exports.BattleScripts = {
 				ability: 'Cloud Nine',
 				item: 'Leftovers',
 				gender: 'M',
-				moves: ['Scald', 'Ice Beam', 'Psychic'],
+				moves: ['Scald', 'Ice Beam', 'Psychic',],
 				signatureMove: 'Duck Power',
 				evs: {
 					spa: 252,
 					spe: 252,
-					hp: 4
+					hp: 4,
 				},
 				nature: 'Modest',
 			},
@@ -176,12 +176,12 @@ exports.BattleScripts = {
 				ability: 'Sturdy',
 				item: 'Leftovers',
 				gender: 'M',
-				moves: ['Psychic', 'Aura Sphere', 'Dark Pulse'],
+				moves: ['Psychic', 'Aura Sphere', 'Dark Pulse',],
 				signatureMove: 'Saber Strike',
 				evs: {
 					spa: 252,
 					spe: 252,
-					spd: 4
+					spd: 4,
 				},
 				nature: 'Timid',
 			},
@@ -190,12 +190,12 @@ exports.BattleScripts = {
 				ability: 'Prankster',
 				item: 'Eviolite',
 				gender: 'M',
-				moves: ['Thunder Wave', 'Mean Look', 'Confuse Ray'],
+				moves: ['Thunder Wave', 'Mean Look', 'Confuse Ray',],
 				signatureMove: 'Troll',
 				evs: {
 					hp: 252,
 					def: 252,
-					spd: 4
+					spd: 4,
 				},
 				nature: 'Impish',
 			},
@@ -204,12 +204,12 @@ exports.BattleScripts = {
 				species: 'Registeel',
 				ability: 'No Guard',
 				item: 'Weakness Policy',
-				moves: ['Zap Cannon', 'Iron Head', 'Stone Edge'],
+				moves: ['Zap Cannon', 'Iron Head', 'Stone Edge',],
 				signatureMove: 'Spacial Blast',
 				evs: {
 					atk: 252,
 					spd: 252,
-					hp: 4
+					hp: 4,
 				},
 				nature: 'Adamant',
 			},
@@ -218,13 +218,13 @@ exports.BattleScripts = {
 				ability: 'Wonder Guard',
 				item: 'Leftovers',
 				moves: [
-					['Stone Edge', 'Earthquake'][variant], 'Explosion', 'Iron Head'
+					['Stone Edge', 'Earthquake'][variant], 'Explosion', 'Iron Head',
 				],
 				signatureMove: 'Ancient Ritual',
 				evs: {
 					atk: 252,
 					spd: 252,
-					hp: 4
+					hp: 4,
 				},
 				nature: 'Adamant',
 			},
@@ -232,12 +232,12 @@ exports.BattleScripts = {
 				species: 'Regice',
 				ability: 'Flash Fire',
 				item: 'Leftovers',
-				moves: ['Ice Beam', 'Ancient Power', 'Thunderbolt'],
+				moves: ['Ice Beam', 'Ancient Power', 'Thunderbolt',],
 				signatureMove: 'Frostbite',
 				evs: {
 					spa: 252,
 					spd: 252,
-					hp: 4
+					hp: 4,
 				},
 				nature: 'Adamant',
 			},
@@ -246,12 +246,12 @@ exports.BattleScripts = {
 				species: 'Mew',
 				ability: 'Protean',
 				item: 'Expert Belt',
-				moves: ['Earth Power', 'Oblivion Wing', 'Shadow Ball'],
+				moves: ['Earth Power', 'Oblivion Wing', 'Shadow Ball',],
 				signatureMove: 'Mewtation',
 				evs: {
 					spa: 252,
 					spe: 252,
-					hp: 4
+					hp: 4,
 				},
 				nature: 'Timid',
 			},
@@ -261,12 +261,12 @@ exports.BattleScripts = {
 				ability: 'Water Absorb',
 				item: 'Leftovers',
 				gender: 'M',
-				moves: ['Swords Dance', 'Sacred Sword', 'Shadow Sneak', 'Kings Shield'],
+				moves: ['Swords Dance', 'Sacred Sword', 'Shadow Sneak', 'Kings Shield',],
 				signatureMove: 'Zeo-Bash',
 				evs: {
 					atk: 252,
 					spd: 8,
-					hp: 248
+					hp: 248,
 				},
 				nature: 'Adamant',
 			},
@@ -275,12 +275,12 @@ exports.BattleScripts = {
 				ability: 'Solar Power',
 				item: 'Charizardite X',
 				gender: 'M',
-				moves: ['Outrage', 'Flare Blitz', 'Thunder Punch'],
+				moves: ['Outrage', 'Flare Blitz', 'Thunder Punch',],
 				signatureMove: 'Nature\'s Fury',
 				evs: {
 					spa: 252,
 					spe: 252,
-					hp: 4
+					hp: 4,
 				},
 				nature: 'Jolly',
 			},
@@ -288,12 +288,12 @@ exports.BattleScripts = {
 				species: 'Flygon',
 				ability: 'Levitate',
 				item: 'Choice Band',
-				moves: ['Outrage', 'Dragon Dance', 'Earthquake'],
+				moves: ['Outrage', 'Dragon Dance', 'Earthquake',],
 				signatureMove: 'Out Ripper',
 				evs: {
 					atk: 252,
 					spe: 252,
-					hp: 4
+					hp: 4,
 				},
 				nature: 'Adamant',
 			},
@@ -301,12 +301,12 @@ exports.BattleScripts = {
 				species: 'Volcanion',
 				ability: 'Protean',
 				item: 'Life Orb',
-				moves: ['Fire Blast', 'Steam Eruption', 'Recover'],
+				moves: ['Fire Blast', 'Steam Eruption', 'Recover',],
 				signatureMove: 'Vapor Boost',
 				evs: {
 					spa: 252,
 					spe: 252,
-					hp: 4
+					hp: 4,
 				},
 				nature: 'Timid',
 			},
@@ -315,12 +315,12 @@ exports.BattleScripts = {
 				ability: 'Guts',
 				item: 'Flame Orb',
 				gender: 'M',
-				moves: ['Fire Punch', 'Thunder Punch', 'Facade'],
+				moves: ['Fire Punch', 'Thunder Punch', 'Facade',],
 				signatureMove: 'Vacation Time',
 				evs: {
 					atk: 252,
 					spe: 4,
-					hp: 252
+					hp: 252,
 				},
 				nature: 'Modest',
 			},
@@ -328,13 +328,13 @@ exports.BattleScripts = {
 				species: 'Porygon2',
 				ability: 'Prankster',
 				item: 'Eviolite',
-				moves: ['Tri Attack', 'Recover', 'Ice Beam'],
+				moves: ['Tri Attack', 'Recover', 'Ice Beam',],
 				signatureMove: 'Xavier Hax',
 				evs: {
 					hp: 252,
 					def: 136,
 					spa: 24,
-					spd: 96
+					spd: 96,
 				},
 				nature: 'Bold',
 			},
@@ -343,12 +343,12 @@ exports.BattleScripts = {
 				ability: 'Gale Wings',
 				item: 'Rocky Helmet',
 				gender: 'M',
-				moves: ['Aura Sphere', 'Taunt', 'Hurricane'],
+				moves: ['Aura Sphere', 'Taunt', 'Hurricane',],
 				signatureMove: 'Rainbust Orb',
 				evs: {
 					spa: 252,
 					spe: 252,
-					hp: 4
+					hp: 4,
 				},
 				nature: 'Bold',
 			},
@@ -357,12 +357,12 @@ exports.BattleScripts = {
 				ability: 'Guts',
 				item: 'Leftovers',
 				gender: 'M',
-				moves: ['Substitute', 'Dragon Dance', 'Bounce'],
+				moves: ['Substitute', 'Dragon Dance', 'Bounce',],
 				signatureMove: 'Tsunami Crash',
 				evs: {
 					atk: 252,
 					spe: 252,
-					hp: 4
+					hp: 4,
 				},
 				nature: 'Adamant',
 			},
@@ -371,18 +371,18 @@ exports.BattleScripts = {
 				ability: 'Unburden',
 				item: 'White Herb',
 				gender: 'M',
-				moves: ['Drain Punch', 'Stone Edge', 'Megahorn'],
+				moves: ['Drain Punch', 'Stone Edge', 'Megahorn',],
 				signatureMove: 'Shell Break',
 				evs: {
 					atk: 128,
 					spe: 128,
-					hp: 252
+					hp: 252,
 				},
 				nature: 'Adamant',
 			},
 		};
 		// convert moves to ids.
-		for (var k in sets) {
+		for (let k in sets) {
 			sets[k].moves = sets[k].moves.map(toId);
 			sets[k].baseSignatureMove = toId(sets[k].baseSignatureMove);
 		}
@@ -401,29 +401,31 @@ exports.BattleScripts = {
 					def: 31,
 					spa: 31,
 					spd: 31,
-					spe: 31
+					spe: 31,
 				};
 			} else {
 				for (let iv in {
-						hp: 31,
-						atk: 31,
-						def: 31,
-						spa: 31,
-						spd: 31,
-						spe: 31
-					}) {
+					hp: 31,
+					atk: 31,
+					def: 31,
+					spa: 31,
+					spd: 31,
+					spe: 31,
+				}) {
 					set.ivs[iv] = iv in set.ivs ? set.ivs[iv] : 31;
 				}
 			}
 			// Assuming the hardcoded set evs are all legal.
-			if (!set.evs) set.evs = {
+			if (!set.evs) {
+			set.evs = {
 				hp: 84,
 				atk: 84,
 				def: 84,
 				spa: 84,
 				spd: 84,
-				spe: 84
-			};
+				spe: 84,
+			}
+		};
 			set.moves = [this.sampleNoReplace(set.moves), this.sampleNoReplace(set.moves), this.sampleNoReplace(set.moves)].concat(set.signatureMove);
 			team.push(set);
 		}
