bacf7934a1758ece6f913d22fc877c38053dab8a
Lord-Haji <lordhajisinbox@gmail.com>
2016-12-18 12:47:40 +0530

    refactor eShop.js

--- chat-plugins/eShop.js
+++ chat-plugins/eShop.js
@@ -2,6 +2,7 @@
     Default shop setup
     for spacialgaze.psim.us
     coded by HoeenHero
+    Refactored by Lord Haji
     -----------------------
     THIS IS NOT THE SAME AS
     THE PRIVATE SHOP ON SG
@@ -64,121 +65,90 @@ try {
 	allowThisShop = true;
 }
 
-exports.commands = {
+exports.commands = {	
 	//shop: 'eshop', //Uncomment this if you want this to be able to be used using the /shop command
 	eshop: {
 		add: function (target, room, user, connection, cmd, message) {
 			if (!this.can('roomowner')) return false;
-			if (!allowThisShop) return this.errorReply('This shop is closed');
-			if (SG.eShop.closed) return this.sendReply('An error closed the shop.');
+			if (Shop.closed) return this.sendReply('An error closed the shop.');
 			target = target.split(',');
-			if (!target[2]) return this.parse('/eshop help');
-			if (SG.eShop[toId(target[0])]) return this.errorReply(target[0] + ' is already in the shop.');
-			if (isNaN(Number(target[2]))) return this.parse('/eshop help');
-			let isSSB = false;
-			if (toId(target[0]) === 'shiny' || toId(target[0]) === 'ffacustomsymbol' || toId(target[0]) === 'customability' || toId(target[0]) === 'customitem' || toId(target[0]) === 'custommove') isSSB = true;
-			SG.eShop[toId(target[0])] = new NewItem(target[0], target[1], target[2], isSSB);
+			if (!target[2]) return this.parse('/shop help');
+			if (Shop[toId(target[0])]) return this.errorReply(target[0] + ' is already in the shop.');
+			if (isNaN(Number(target[2]))) return this.parse('/shop help');
+			Shop[toId(target[0])] = new NewItem(target[0], target[1], target[2]);
 			writeShop();
 			return this.sendReply('The item ' + target[0] + ' was added.');
 		},
+		delete: 'remove',
 		remove: function (target, room, user, connection, cmd, message) {
-			if (!allowThisShop) return this.errorReply('This shop is closed');
 			if (!this.can('roomowner')) return false;
-			if (SG.eShop.closed) return this.sendReply('An error closed the shop.');
-			if (!target) return this.parse('/eshop help');
-			if (!SG.eShop[toId(target)]) return this.errorReply(target + ' is not in the shop.');
-			delete SG.eShop[toId(target)];
+			if (Shop.closed) return this.sendReply('An error closed the shop.');
+			if (!target) return this.parse('/shop help');
+			if (!Shop[toId(target)]) return this.errorReply(target + ' is not in the shop.');
+			delete Shop[toId(target)];
 			writeShop();
 			return this.sendReply('The item ' + target + ' was removed.');
 		},
 		buy: function (target, room, user, connection, cmd, message) {
-			if (!allowThisShop) return this.errorReply('This shop is closed');
-			if (!target) return this.parse('/eshop help buy');
-			if (SG.eShop.closed) return this.sendReply('The shop is closed, come back later.');
-			if (!SG.eShop[toId(target)]) return this.errorReply('Item ' + target + ' not found.');
-			let item = SG.eShop[toId(target)];
-			Economy.readMoney(user.userid, userMoney => {
-				if (item.price > userMoney) return this.errorReply('You need ' + (item.price - userMoney) + ' more ' + ((item.price - userMoney) === 1 ? global.currencyName : global.currenyPlural) + ' to buy this.');
-				if (item.isSSB && !SG.ssb[user.userid]) return this.sendReply('You need to run /ssb edit at least once before you can buy this.');
-				if (item.isSSB) {
-					//handle SSB pre-buy events
-					switch (item.id) {
-					case 'shiny':
-						if (SG.ssb[user.userid].canShiny) return this.sendReply('You already own this.');
-						break;
-					case 'ffacustomsymbol':
-						if (SG.ssb[user.userid].cSymbol) return this.sendReply('You already own this.');
-						if (user.isStaff || user.group === '+') {
-							//give free
-							SG.ssb[user.userid].cSymbol = true;
-							return this.sendReply('Because you have global auth you have been given this for free!');
-						}
-						break;
-					case 'customitem':
-						if (SG.ssb[user.userid].bought.cItem) return this.sendReply('You already own this.');
-						break;
-					case 'customability':
-						if (SG.ssb[user.userid].bought.cAbility) return this.sendReply('You already own this.');
-						break;
-					case 'custommove':
-						if (SG.ssb[user.userid].bought.cMove) return this.sendReply('You already own this.');
-						break;
-					default:
-						//Unhandled
-					}
+			if (!target) return this.parse('/shop help buy');
+			if (Shop.closed) return this.sendReply('The shop is closed, come back later.');
+			if (!Shop[toId(target)]) return this.errorReply('Item ' + target + ' not found.');
+			let item = Shop[toId(target)];
+			if (item.price > Db('money').get(user.userid)) return this.errorReply("You don't have you enough money for this. You need " + (item.price - Db('money').get(user.userid)) + SG.currencyName((item.price - Db('money').get(user.userid))) + " more to buy this.");
+			Db('money').set(user.userid, Db('money').get(user.userid) - item.price);
+			SG.logMoney(user.name + " has purchased " + item.name + " from the shop for " + item.price + " and " + user.name + " now has " + Db('money').get(user.userid) + SG.currencyName(Db('money').get(user.userid)) + ".");
+			if (item.id === 'customsymbol') {
+				user.canCustomSymbol = true;
+			}
+			let msg = '**' + user.name + " has bought " + item.name + ".** for " + item.price + SG.currencyName(item.price) + " and now has " + Db('money').get(user.userid) + SG.currencyName(Db('money').get(user.userid)) + ".";
+			Rooms.rooms.get("staff").add('|c|~Shop Alert|' + msg);
+			Rooms.rooms.get("staff").update();
+			Users.users.forEach(function (user) {
+				if (user.group === '~' || user.group === '&') {
+					user.send('|pm|~Shop Alert|' + user.getIdentity() + '|' + msg);
 				}
-				Economy.writeMoney(user.userid, item.price * -1, () => {
-					Economy.readMoney(user.userid, amount => {
-						Economy.logTransaction(user.name + " has purchased a " + item.name + " for " + item.price + " " + (item.price === 1 ? global.currencyName : global.currenyPlural) + ". They now have " + amount + " " + (userMoney === 1 ? global.currencyName : global.currenyPlural));
-						switch (item.id) {
-						case 'customsymbol':
-							user.canCustomSymbol = true;
-							break;
-						case 'shiny':
-							SG.ssb[user.userid].canShiny = true;
-							writeSSB();
-							break;
-						case 'ffacustomsymbol':
-							SG.ssb[user.userid].cSymbol = true;
-							writeSSB();
-							break;
-						case 'customitem':
-							SG.ssb[user.userid].bought.cItem = true;
-							writeSSB();
-							break;
-						case 'customability':
-							SG.ssb[user.userid].bought.cAbility = true;
-							writeSSB();
-							break;
-						case 'custommove':
-							SG.ssb[user.userid].bought.cMove = true;
-							writeSSB();
-							break;
-						default:
-							SG.messageSeniorStaff(user.name + ' has purchased a ' + item.name + '.');
-						}
-						user.sendTo(room, "|uhtmlchange|eshop" + user.userid + "|<div style='max-height:300px'><table style='border:2px solid #101ad1; border-radius: 5px'><tr><th colspan='3' style='border: 2px solid #070e96; border-radius: 5px'>Server Shop</th></tr><tr><td style='colspan: 3; border: 2px solid #070e96; border-radius: 5px'><center>You have purchased a " + item.name + ". " + (item.id === 'customsymbol' ? "You may now use /customsymbol [symbol] to change your symbol." : "Upper staff have been notified of your purchase and will contact you shortly.") + "</center></td></tr><tr><td colspan='3' style='text-align:center'><button name='send' value='/eshop reopen'>Return to Shop</button></td></tr></table>");
-					});
-				});
 			});
+			user.sendTo(room, "|uhtmlchange|shop" + user.userid + "|<div style='max-height:300px'><table style='border:2px solid #000000; border-radius: 5px'><tr><th colspan='3' style='border: 2px solid #000000; border-radius: 5px'>Server Shop</th></tr><tr><td style='colspan: 3; border: 2px solid #000000; border-radius: 5px'><center>You have purchased a " + item.name + ". " + (item.id === 'customsymbol' ? "You may now use /customsymbol [symbol] to change your symbol." : "Upper staff have been notified of your purchase and will contact you shortly.") + "</center></td></tr><tr><td colspan='3' style='text-align:center'><button class='button' name='send' value='/shop reopen'>Return to Shop</button></td></tr></table>");
 		},
 		help: function (target, room, user, connection, cmd, message) {
 			let reply = '<b>Shop commands</b><br/>';
-			reply += '/eshop - Load the shop screen.<br/>';
-			reply += '/eshop buy [item] - Buy an item from the shop.<br/>';
+			reply += '/shop - Load the shop screen.<br/>';
+			reply += '/shop buy [item] - Buy an item from the shop.<br/>';
 			if (user.can('roomowner')) {
 				reply += '<b>Administrative shop commands:</b><br/>';
-				reply += '/eshop add [item name], [description], [price], (is a SSBFFA item) - Adds a item to the shop.<br/>';
-				reply += 'Valid SSBFFA items are: shiny, ffacustommove, customitem, customability, custommove.<br/>';
-				reply += '/eshop remove [item] - removes a item from the shop.<br/>';
+				reply += '/shop add [item name], [description], [price] - Adds a item to the shop.<br/>';
+				reply += '/shop remove [item] - removes a item from the shop.<br/>';
 			}
 			return this.sendReplyBox(reply);
 		},
 		reopen: '',
 		'': function (target, room, user, connection, cmd, message) {
-			if (!allowThisShop) return this.errorReply('This shop is closed');
-			if (cmd === 'reopen') return user.sendTo(room, '|uhtmlchange|eshop' + user.userid + '|' + shopDisplay());
-			return user.sendTo(room, '|uhtml|eshop' + user.userid + '|' + shopDisplay());
+			if (cmd === 'reopen') return user.sendTo(room, '|uhtmlchange|Shop' + user.userid + '|' + shopDisplay());
+			return user.sendTo(room, '|uhtml|shop' + user.userid + '|' + shopDisplay());
 		},
 	},
+
+	customsymbol: function (target, room, user) {
+		let bannedSymbols = ['!', '|', '‽', '\u2030', '\u534D', '\u5350', '\u223C'];
+		for (let u in Config.groups) if (Config.groups[u].symbol) bannedSymbols.push(Config.groups[u].symbol);
+		if (!user.canCustomSymbol) return this.errorReply('You need to buy this item from the shop to use.');
+		if (!target || target.length > 1) return this.sendReply('/customsymbol [symbol] - changes your symbol (usergroup) to the specified symbol. The symbol can only be one character');
+		if (target.match(/([a-zA-Z ^0-9])/g) || bannedSymbols.indexOf(target) >= 0) {
+			return this.sendReply('Sorry, but you cannot change your symbol to this for safety/stability reasons.');
+		}
+		user.customSymbol = target;
+		user.updateIdentity();
+		user.canCustomSymbol = false;
+		this.sendReply('Your symbol is now ' + target + '. It will be saved until you log off for more than an hour, or the server restarts. You can remove it with /resetsymbol');
+	},
+	customsymbolhelp: ["/customsymbol [symbol] - Get a custom symbol."],
+
+	removesymbol: 'resetsymbol',
+	resetsymbol: function (target, room, user) {
+		if (!user.customSymbol) return this.errorReply("You don't have a custom symbol!");
+		delete user.customSymbol;
+		user.updateIdentity();
+		this.sendReply('Your symbol has been removed.');
+	},
+	resetsymbolhelp: ["/resetsymbol - Resets your custom symbol."],
 };
