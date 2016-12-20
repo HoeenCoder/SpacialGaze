2077f247251fd1579c44060f8de5d873b0c43d6b
Lord-Haji <lordhajisinbox@gmail.com>
2016-12-20 22:54:36 +0530

    Update customavatar.js

--- chat-plugins/customavatar.js
+++ chat-plugins/customavatar.js
@@ -65,8 +65,8 @@ exports.commands = {
 			Config.customavatars[name] = name + ext;
 
 			download_image(AVATAR, name, ext);
-			this.sendReply(parts[0] + "'s avatar has been set.");
-			Users.get(name).popup("|html|" + SG.nameColor(user.name, true) + " set your custom avatar.<br /><img src='" + AVATAR_URL + "' width='80' height='80'><br /> Refresh your page if you don\'t see it.");
+			this.sendReply("|raw|" + name + "'s avatar has been set to: <img src='" + AVATAR_URL + "' width='80' height='80'> Successfully. ");
+			Users.get(name).popup("|html|" + SG.nameColor(user.name, true) + " set your custom avatar.<br /><center><img src='" + AVATAR_URL + "' width='80' height='80'></center><br /> Refresh your page if you don\'t see it.");
 		},
 
 		remove: 'delete',
