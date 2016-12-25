df4ed26ceab7bcbd07407be99dc1e8788884eebf
Lord-Haji <lordhajisinbox@gmail.com>
2016-12-25 20:03:24 +0530

    Misc: Add /autorank

--- spacialgaze-plugins/misc.js
+++ spacialgaze-plugins/misc.js
@@ -78,6 +78,7 @@ exports.commands = {
 		if (!buffer.length) return connection.popup("This server has no global authority.");
 		connection.send("|popup||html|" + buffer.join("\n\n"));
 	},
+	
 	roomauthority: 'roomauthlist',
 	roomstaff: 'roomauthlist',
 	roomauth: 'roomauthlist',
@@ -114,6 +115,58 @@ exports.commands = {
 		if (targetRoom !== room) buffer.unshift("" + targetRoom.title + " room auth:");
 		connection.send("|popup||html|" + buffer.join("\n\n") + userLookup);
 	},
+	
+	autovoice: 'autorank',
+	autodriver: 'autorank',
+	autobot: 'autorank',
+	automod: 'autorank',
+	autoowner: 'autorank',
+	autopromote: 'autorank',
+	autorank: function (target, room, user, connection, cmd) {
+		switch (cmd) {
+		case 'autovoice':
+			target = '+';
+			break;
+		case 'autobot':
+			target = '*';
+			break;			
+		case 'autodriver':
+			target = '%';
+			break;
+		case 'automod':
+			target = '@';
+			break;
+		case 'autoleader':
+			target = '&';
+			break;			
+		case 'autoowner':
+			target = '#';
+			break;
+		}
+
+		if (!target) return this.sendReply("Usage: /autorank [rank] - Automatically promotes user to the specified rank when they join the room.");
+		if (!this.can('roommod', null, room)) return false;
+		target = target.trim();
+
+		if (target === 'off' && room.autorank) {
+			delete room.autorank;
+			delete room.chatRoomData.autorank;
+			Rooms.global.writeChatRoomData();
+			for (let u in room.users) Users(u).updateIdentity();
+			return this.privateModCommand("(" + user.name + " has disabled autorank in this room.)");
+		}
+		if (room.autorank && room.autorank === target) return this.sendReply("Autorank is already set to \"" + target + "\".");
+
+		if (Config.groups[target] && !Config.groups[target].globalonly) {
+			if (target === '#' && user.userid !== room.founder) return this.sendReply("You can't set autorank to # unless you're the room founder.");
+			room.autorank = target;
+			room.chatRoomData.autorank = target;
+			Rooms.global.writeChatRoomData();
+			for (let u in room.users) Users(u).updateIdentity();
+			return this.privateModCommand("(" + user.name + " has set autorank to \"" + target + "\" in this room.)");
+		}
+		return this.sendReply("Group \"" + target + "\" not found.");
+	},	
 
 	clearall: function (target, room, user) {
 		if (!this.can('declare')) return false;
