485969550fc2051cdb0773a6edc2ef52e36cdafe
Executive Dronpes <lordhajisinbox@gmail.com>
2017-01-19 23:43:52 +0530

    Update colors.js

--- spacialgaze-plugins/colors.js
+++ spacialgaze-plugins/colors.js
@@ -34,7 +34,7 @@ function updateColor() {
 	let file = fs.readFileSync('config/custom.css', 'utf8').split('\n');
 	if (~file.indexOf('/* COLORS START */')) file.splice(file.indexOf('/* COLORS START */'), (file.indexOf('/* COLORS END */') - file.indexOf('/* COLORS START */')) + 1);
 	fs.writeFileSync('config/custom.css', file.join('\n') + newCss);
-	reloadCSS();
+	SG.reloadCSS();
 }
 
 function generateCSS(name, color) {
