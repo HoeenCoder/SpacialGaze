69889d8bfd88b552683404c9d4e55cfa9c7d84d6
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2017-02-08 18:58:38 +0530

    Update scripts.js

--- mods/metronome/scripts.js
+++ mods/metronome/scripts.js
@@ -1,7 +1,7 @@
 'use strict';
 exports.BattleScripts = {
 	init: function () {
-		for(let i in this.data.Learnsets) {
+		for (let i in this.data.Learnsets) {
 			this.modData('Learnsets', i).learnset.metronome = ['7M'];
 		}
 	},
