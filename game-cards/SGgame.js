f2e08ca1a62b4d23879809abff93eab16d484092
Executive Dronpes <lordhajisinbox@gmail.com>
2017-02-12 23:19:03 +0530

    Do requested changes

--- game-cards/SGgame.js
+++ game-cards/SGgame.js
@@ -234,6 +234,7 @@ exports.box = {
 };
 
 exports.commands = {
+	confirmresetalpha: 'playalpha',
 	resetalpha: 'playalpha',
 	continuealpha: 'playalpha',
 	playalpha: function (target, room, user, connection, cmd) {
@@ -242,10 +243,12 @@ exports.commands = {
 		if (cmd === 'playalpha') {
 			let htm = '<center>';
 			if (Db.players.has(user.userid)) htm += '<button name="send" value="/continuealpha" style="display: block; border: 5px solid #AAA; background: #FFF; font-family: monospace; border-radius: 5px; width: 90%; text-align: left;"><b>CONTINUE</b><br/><br/><span style="color: #4286f4">PLAYER ' + user.name + '<br/><br/>TIME ' + Math.floor(Math.abs(Date.now() - Db.players.get(user.userid).startedOn) / 86400000) + '<br/><br/>POKEDEX ' + Object.keys(Db.players.get(user.userid).pokedex).length + '</span></button>';
-			htm += '<button name="send" value="/resetalpha" style="display: block; border: 5px solid #AAA; background: #FFF; font-family: monospace; border-radius: 5px; width: 90%; text-align: left;"><b>NEW GAME</b></button></center>';
+			htm += '<button name="send" value="/confirmresetalpha" style="display: block; border: 5px solid #AAA; background: #FFF; font-family: monospace; border-radius: 5px; width: 90%; text-align: left;"><b>NEW GAME</b></button></center>';
 			user.console.init();
 			user.console.update('background-color: #6688AA;', htm, null);
 		} else if (cmd === 'resetalpha') {
+			user.console.update(false, '<h2><center>Are You sure ?<br /><button class="button" name="send" value="/confirmresetalpha">Yes</button>   <button class="button" name="send" value="/sggame back">No</button>', false);
+		} else if (cmd === 'confirmresetalpha') {
 			// New Game
 			user.console.curText = ['Welcome to the world of Pokemon!<br/>I\'m HoeenHero, one of the programmers for the game. (click the star to continue)',
 				'Were not done creating the game yet so its limited as to what you can do.<br/>But you can help out by testing whats here, and reporting any issues you find!',
@@ -261,7 +264,7 @@ exports.commands = {
 			}
 			user.console.curText.push(msg + '|hide');
 			user.console.callback = function () {
-				user.console.defaultBottomHTML = '<center><button class="button" name="send" value="/console sound">Toggle Sound</button> <button class="button disabled" name="send" value="/sggame pokedex">Pokedex</button> <button class="button disabled" name="send" value="/sggame pokemon">Pokemon</button> <button class="button disabled" name="send" value="/sggame bag">Bag</button> <button class="button" name="send" value="/sggame pc">PC Boxes</button> <button name="send" value="/search gen7wildpokemonalpha" class="button">Battle!</button> <button name="send" value="/sggame reset" class="button">Reset</button>';
+				user.console.defaultBottomHTML = '<center><button class="button" name="send" value="/console sound">Toggle Sound</button> <button class="button disabled" name="send" value="/sggame pokedex">Pokedex</button> <button class="button disabled" name="send" value="/sggame pokemon">Pokemon</button> <button class="button disabled" name="send" value="/sggame bag">Bag</button> <button class="button" name="send" value="/sggame pc">PC Boxes</button> <button name="send" value="/search gen7wildpokemonalpha" class="button">Battle!</button> <button name="send" value="/resetalpha" class="button">Reset</button>';
 				user.console.callback = null;
 			};
 			user.console.curText.push('Great choice! I\'ll leave you to your game now.|callback');
@@ -269,7 +272,7 @@ exports.commands = {
 			this.parse('/sggame next');
 		} else {
 			// Continue
-			if (!Db.players.has(user.userid)) return this.parse('/resetalpha');
+			if (!Db.players.has(user.userid)) return this.parse('/confirresetalpha');
 			try {
 				Db.players.get(user.userid).test();
 			} catch (e) {
@@ -303,10 +306,6 @@ exports.commands = {
 		back: function (target, room, user) {
 			if (!user.console) return;
 			user.console.update(this.prevScreen[0], this.prevScreen[1], this.prevScreen[2]);
-		},
-		reset: function (target, room, user) {
-			if (!user.console) return;
-			user.console.update(false, '<h2><center>Are You sure ?<br /><button class="button" name="send" value="/resetalpha">Yes</button>   <button class="button" name="send" value="/sggame back">No</button>', false);
 		},
 		pc: function (target, room, user, connection, cmd) {
 			if (!user.console) return;
