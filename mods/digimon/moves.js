44bdcfa521c3b0ccdb4dd203af5bc396636ec743
Insist <DeathlyPlays@users.noreply.github.com>
2017-03-03 07:29:07 -0600

    Digimon Moves

---
+++ mods/digimon/moves.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.BattleScripts = {
+    "bubble": {
+        id: "bubble",
+        name: "Bubble",
+        basePower: 6,
+        category: "Special",
+        secondary: false,
+        priority: 0,
+        target: "any",
+        pp: 40,
+        flags: {},
+        accuracy: 100,
+        type: "Ice",
+    },
+};
