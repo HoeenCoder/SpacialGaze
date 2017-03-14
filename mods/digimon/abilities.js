9656036cc00e54087776906de71df6c18b4b5564
Insist <DeathlyPlays@users.noreply.github.com>
2017-03-14 11:51:11 -0500

    Add the rest of the abilities

--- mods/digimon/abilities.js
+++ mods/digimon/abilities.js
@@ -1,14 +1,9 @@
 'use strict';
 
 exports.BattleAbilities = {
-    "data": {
-        id: "data",
-        name: "Data",
-        onAnyDamage: function (damage, target, source, effect) {
-			if (effect && effect.id === 'data') {
-				return false;
-			}
-		},
+	"data": {
+		id: "data",
+		name: "Data",
 		onBasePowerPriority: 8,
 		onBasePower: function (basePower, attacker, move) {
 			if (attacker.ability) {
@@ -27,5 +22,49 @@ exports.BattleAbilities = {
 				}
 			}
 		},
-    },
+	},
+	"vaccine": {
+		id: "vaccine",
+		name: "Vaccine",
+		onBasePowerPriority: 8,
+		onBasePower: function (basePower, attacker, move) {
+			if (attacker.ability) {
+				if (attacker.ability === 'virus') {
+					this.debug('Vaccine boost');
+					return this.chainModify(1.5);
+				}
+			}
+		},
+		onBasePowerPriority: 8,
+		onBasePower: function (basePower, attacker, move) {
+			if (attacker.ability) {
+				if (attacker.ability === 'data') {
+					this.debug('Vaccine weaken');
+					return this.chainModify(0.5);
+				}
+			}
+		},
+	},
+	"virus": {
+		id: "virus",
+		name: "Virus",
+		onBasePowerPriority: 8,
+		onBasePower: function (basePower, attacker, move) {
+			if (attacker.ability) {
+				if (attacker.ability === 'data') {
+					this.debug('Virus boost');
+					return this.chainModify(1.5);
+				}
+			}
+		},
+		onBasePowerPriority: 8,
+		onBasePower: function (basePower, attacker, move) {
+			if (attacker.ability) {
+				if (attacker.ability === 'vaccine') {
+					this.debug('Virus weaken');
+					return this.chainModify(0.5);
+				}
+			}
+		},
+	},
 };
