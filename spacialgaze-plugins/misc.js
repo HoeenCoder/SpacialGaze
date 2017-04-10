676dd420d62b130084832da3c4c91c89bcf6cc44
Executive Dronpes <lordhajisinbox@gmail.com>
2017-04-10 16:15:28 +0530

    Add intro scroll remover token

--- spacialgaze-plugins/misc.js
+++ spacialgaze-plugins/misc.js
@@ -372,6 +372,11 @@ exports.commands = {
 			msg += '<button class="button" name="send" value="/emote add, ' + target[1] + ', ' + target[2] + '">Add emote</button></center>';
 			delete user.tokens[target[0]];
 			return SG.messageSeniorStaff(msg);
+		case 'disableintroscroll':
+			if (!target[2]) return this.errorReply('/usetoken disableintroscroll, [room]');
+			target[1] = toId(target[1]);
+			msg += '/html <center>' + SG.nameColor(user.name, true) + ' has redeemed roomintro scroll disabler token.<br/>";
+			msg += '<button class="button" name="send" value="/disableintroscroll ' + target[1] + '">Disable Intro Scrool for ' + Rooms(target[1]) + '</button></center>';
 		default:
 			return this.errorReply('An error occured in the command.'); // This should never happen.
 		}
