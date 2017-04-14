4aeef2986d238b14f0be4b9a8588edf3e16ac45a
Eelek <notquitesixty@gmail.com>
2017-04-14 19:05:51 -0400

    Update scripts.js

--- mods/sgssb/scripts.js
+++ mods/sgssb/scripts.js
@@ -425,7 +425,7 @@ exports.BattleScripts = {
 				ability: "Serene Grace",
 				item: "Assault Vest",
 				gender: "M",
-				moves: ['Zing Zap', 'Icicle Crash', 'Flame Charge'
+				moves: ['Zing Zap', 'Icicle Crash', 'Flame Charge',
 				],
 				signatureMove: "Dice Roll",
 				evs: {
