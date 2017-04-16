102b2184d47f4273273bc3cf1f8b3f46a9b8b6c5
Executive Dronpes <lordhajisinbox@gmail.com>
2017-04-16 12:47:41 +0530

    Update misc.js

--- spacialgaze-plugins/misc.js
+++ spacialgaze-plugins/misc.js
@@ -376,7 +376,7 @@ exports.commands = {
 			if (!target[2]) return this.errorReply('/usetoken disableintroscroll, [room]');
 			let roomid = toId(target[1]);
 			if (!Rooms[roomid]) return this.errorReply(`${roomid} is not a room.`);
-			if (Db.DisabledScrolls.has(roomid)) return this.errorReply(`${Rooms(roomid).title} has already roomintro scroll disabled.`);
+			if (Db.disabledScrolls.has(roomid)) return this.errorReply(`${Rooms(roomid).title} has already roomintro scroll disabled.`);
 			msg += '/html <center>' + SG.nameColor(user.name, true) + ' has redeemed roomintro scroll disabler token.<br/>';
 			msg += '<button class="button" name="send" value="/disableintroscroll ' + target[1] + '">Disable Intro Scrool for <b>' + Rooms(roomid).title + '</b></button></center>';
 			delete user.tokens[target[0]];
