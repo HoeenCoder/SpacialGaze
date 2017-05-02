9941324f806f3fd287dcad7f41a048f0da9ad721
VXN <notquitesixty@gmail.com>
2017-05-02 09:48:36 -0400

    Update moves.js

--- mods/Seasonal/moves.js
+++ mods/Seasonal/moves.js
@@ -1,12 +1,20 @@
-"growth": {
+growth: {
+		num: 74,
 		accuracy: true,
 		basePower: 0,
 		category: "Status",
-		desc: "Raises the user's Attack and Special Attack by 1 stage. If the weather is Sunny Day, raises the user's Attack and Special Attack by 2 stages.",
-		shortDesc: "Raises user's Attack and Sp. Atk by 1; 2 in Sun.",
 		id: "growth",
-    onModifyMove: function (move){
-    switch (this.random(7)) {
+		name: "Growth",
+		pp: 20,
+		priority: 0,
+		flags: {snatch: 1},
+		secondary: false,
+		target: "self",
+		type: "Normal",
+		contestType: "Beautiful",
+	},
+onModifyMove: function (move) {
+				switch (this.random(7)) {
 				case 0:
 					move.category = 'Special';
 					move.type = 'Fire';
@@ -79,15 +87,4 @@
 				}
 			}
 		},
-    
-    
-    },
-		name: "Growth",
-		pp: 20,
-		priority: 0,
-		flags: {},
-		secondary: false,
-		target: "self",
-		type: "Normal",
-		contestType: "Beautiful",
 	},
