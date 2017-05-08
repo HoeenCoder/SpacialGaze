168806af047f0a0541b61abf3e46ef82e49de856
HoeenHero <HoeenCoder@users.noreply.github.com>
2017-05-08 17:15:58 -0400

    Update roomrequest.js

--- spacialgaze-plugins/roomrequest.js
+++ spacialgaze-plugins/roomrequest.js
@@ -52,26 +52,106 @@ exports.commands = {
 		switch (toId(target[0])) {
 		case '':
 		case 'view':
-			
+			let requests = Db.rooms.keys();
+			let output = `<div class="infobox infobox-limited"><table><tr><th style="border: 1px solid" colspan="6"><b>Spacial Gaze Room Requests</b></th></tr><tr><th style="border: 1px solid">Requester</th><th style="border: 1px solid">Room Name</th><th style="border: 1px solid">Room Type</th><th style="border: 1px solid">Description</th><th style="border: 1px solid">Status</th><th style="border: 1px solid">Options</th></tr>`;
+			for (let key in requests) {
+				let cur = Db.rooms.get(key);
+				output += `<tr><td style="border: 1px solid">${key}</td><td style="border: 1px solid">${cur.name}</td><td style="border: 1px solid">${cur.type}</td><td style="border: 1px solid">${cur.desc}</td><td style="border: 1px solid">${cur.status}</td>`;
+				if (cur.status === 'pending') {
+					output += `<td style="border: 1px solid"><button class="button" name="send" value="/roomrequests accept, ${key}">Accept</button><button class="button" name="send" value="/roomrequests reject, ${key}">Reject</button></td></tr>`;
+				} else {
+					output += `<td style="border: 1px solid">${cur.status} by ${cur.by}</td></tr>`;
+				}
+			}
+			output += `</table></div>`;
+			return user.sendTo(room, `|html|${output}`);
 			break;
 		case 'accept':
-			
+			if (!target[1]) return this.parse('/help roomrequest');
+			let req = Db.rooms.get(toId(target[1]));
+			if (!req) return this.errorReply(`${target[1]} does not have a room request.`);
+			if (req.blacklisted) return this.errorReply(`${target[1]} is banned from owning rooms.`);
+			if (req.status !== 'pending') return this.errorReply(`${target[1]}'s current request has already been ${req.status}.`);
+			req.status = 'accepted';
+			req.by = user.userid;
+			req.cooldown = Date.now();
+			Db.rooms.set(toId(target[1]), req);
+			this.parse(`/${(req.type === 'public' ? `makechatroom` : `makeprivatechatroom`)} ${req.name}`);
+			user.popup(`|html|<center>You accepted the room request from ${target[1]}.<br/>The Room "${req.name}" was created, make sure to appoint ${target[1]} room founder!</center>`);
+			return this.parse(`/join ${req.name}`);
 			break;
 		case 'reject':
-			
+			if (!target[1]) return this.parse('/help roomrequest');
+			let req = Db.rooms.get(toId(target[1]));
+			if (!req) return this.errorReply(`${target[1]} does not have a room request.`);
+			if (req.blacklisted) return this.errorReply(`${target[1]} is banned from owning rooms.`);
+			if (req.status !== 'pending') return this.errorReply(`${target[1]}'s current request has already been ${req.status}.`);
+			req.status = 'rejected';
+			req.by = user.userid;
+			req.cooldown = Date.now();
+			Db.rooms.set(toId(target[1]), req);
+			this.sendReply(`You rejected the room request from ${target[1]}`);
 			break;
 		case 'delete':
-			
+			if (!target[1]) return this.parse('/help roomrequest');
+			let req = Db.rooms.get(toId(target[1]));
+			if (!req) return this.errorReply(`${target[1]} does not have a room request.`);
+			if (req.blacklisted) return this.errorReply(`${target[1]} is banned from owning rooms. If you want to undo the blacklist do /roomrequests unblacklist, ${target[1]}`);
+			Db.rooms.set(toId(target[1]), undefined);
+			this.sendReply(`You deleted the room request from ${target[1]}`);
 			break;
 		case 'blacklist':
-			
+			if (!target[1]) return this.parse('/help roomrequest');
+			target[1] = toId(target[1]);
+			let targetUser = Users(target[1]);
+			let req = Db.rooms.get(target[1]);
+			if (req && req.blacklisted) return this.errorReply(`${target[1]} is already banned from owning rooms.`);
+			Db.rooms.set(target[1], {blacklisted: true, by: user.userid, reason: (target[2] || undefined)});
+			let demoted = [];
+			Rooms.rooms.forEach((curRoom, id) => {
+				if (!curRoom.auth || !curRoom.auth[target[1]]) return;
+				if (curRoom.founder === target[1]) {
+					curRoom.founder = false;
+					curRoom.chatRoomData.founder = false;
+				}
+				if (curRoom.auth[target[1]] === '#') {
+					curRoom.auth[target[1]] = '@';
+					demoted.push(curRoom.id);
+					if (targetUser) room.onUpdateIdentity(targetUser);
+				}
+			});
+			if (demoted.length) Rooms.global.writeChatRoomData();
+			if (targetUser) targetUser.popup(`|html|<center>${user.name} has banned you from owning rooms. (${target[2]})<br/>You have been automatcally demoted from room owner in all rooms you had it in (if any).<br/>To appeal your room ownership blacklist, PM a & or ~.</center>`);
+			if (Rooms('upperstaff')) Monitor.adminlog(`${target[1]} was banned from owning rooms by ${user.name} ${(demoted.length ? `and demoted from # in ${demoted.join(', ')}` : ``)}. ${(target[2] ? `(${target[2]})` : ``)}`);
+			return this.sendReply(`${target[1]} was banned from owning rooms.`);
 			break;
 		case 'unblacklist':
-			
+			if (!target[1]) return this.parse('/help roomrequest');
+			target[1] = toId(target[1]);
+			let req = Db.rooms.get(target[1]);
+			if (!req || !req.blacklisted) return this.errorReply(`${target[1]} is not banned from owning rooms.`);
+			Db.rooms.set(target[1], undefined);
+			if (Rooms('upperstaff')) Monitor.adminlog(`${target[1]} was unbanned from owning rooms by ${user.name}.`);
+			return this.sendReply(`${target[1]} is no longer banned from owning rooms.`);
+			break;
+		case 'viewblacklist':
+			if (target[1]) {
+				target[1] = toId(target[1]);
+				let req = Db.rooms.get(target[1]);
+				if (!req || !req.blacklisted) return this.errorReply(`${target[1]} is not banned from owning rooms.`);
+				return this.sendReply(`ROOMOWNERBAN by ${req.by} ${(req.reason ? `(${req.reason})` : ``)}.`);
+			}
+			let list = [];
+			let keys = Db.rooms.keys();
+			for (let key in keys) {
+				if (key.blacklisted) list.push(key);
+			}
+			if (!list.length) return this.sendReply('No users are banned from owning rooms.');
+			return this.sendReply(`The following ${list.length} users are banned from owning rooms: ${list.join(', ')}.`);
 			break;
 		default:
 			return this.parse('/help roomrequest');
 		}
 	},
-	roomrequestshelp: ["/roomrequests [view|accept|reject|delete|blacklist|unblacklist], [request] - Display and manage the list of current room requests. Requires: &, ~"],
+	roomrequestshelp: ["/roomrequests [view|accept|reject|delete|blacklist|unblacklist|viewblacklist], [request|username], (reason if blacklist) - Display and manage the list of current room requests. Requires: &, ~"],
 };
