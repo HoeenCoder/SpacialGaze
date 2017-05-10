4ffddfbc25ab3b3bffc9f356b1acc649acffa5cc
Insist <DeathlyPlays@users.noreply.github.com>
2017-05-10 15:15:56 -0700

    SGSSB: Almighty Bronzong's Ability

--- mods/sgssb/abilities.js
+++ mods/sgssb/abilities.js
@@ -27,4 +27,24 @@ exports.BattleAbilities = {
 			this.setTerrain('');
 		},
 	},
+	conflictofinterest: {
+		id: "conflictofinterest",
+		name: "Conflict of Interest",
+		//Since levitate cant be coded in
+		onStart: function (pokemon) {
+			this.useMove('magnetrise', pokemon);
+		},
+		//HeatProof and fliter other types
+		onBasePowerPriority: 7,
+		onSourceBasePower: function (basePower, attacker, defender, move) {
+			if (move.type === 'Fire' || move.type === 'Ghost' || move.type === 'Dark') {
+				return this.chainModify(0.5);
+			}
+		},
+		onDamage: function (damage, target, source, effect) {
+			if (effect && effect.id === 'brn') {
+				return damage / 2;
+			}
+		},
+	},
 };
