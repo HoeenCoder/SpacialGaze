855ca5d418bc77e8ec40143c83882402d0ed5348
HoeenHero <HoeenCoder@users.noreply.github.com>
2017-05-12 10:26:37 -0400

    Update items.js

--- mods/sgssb/items.js
+++ mods/sgssb/items.js
@@ -6,11 +6,10 @@ exports.BattleItems = {
 		onTakeItem: false,
 		id: "playniumz",
 		name: "Playnium Z",
+		isNonStandard: true,
 		zMove: "Exiled From All Others",
 		zMoveFrom: "Aqua Subscribe",
 		zMoveUser: ["Ludicolo"],
-		num: -1,
-		gen: -1,
 		desc: "If holder is a Ludicolo with Aqua Subscribe, it can use Exiled From All Others.",
 	},
 };
