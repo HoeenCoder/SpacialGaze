e137c583cc65b43a483f25759c17c8bc4dd9ff7e
VXN <notquitesixty@gmail.com>
2017-05-15 20:41:07 -0400

    Update scripts.js

--- mods/seasonal/scripts.js
+++ mods/seasonal/scripts.js
@@ -4,8 +4,8 @@ exports.BattleScripts = {
 	pokemon:{
 		moveUsed(move) {
 			this.lastMove = this.battle.getMove(move).id;
-			for (let i=0;i<this.moveset.length;i++) {
-				if (move.id == this.moveset[i].id) {
+			for (let i = 0; i < this.moveset.length; i++) {
+				if (move.id === this.moveset[i].id) {
 					this.lastM = this.moveset[i];
 					this.lastM.disabled = false;
 					this.lastM.pp = this.lastM.maxpp;
@@ -13,6 +13,6 @@ exports.BattleScripts = {
 				}
 			}
 			this.moveThisTurn = this.lastMove;
-		}
-	}
+		},
+	},
 };
