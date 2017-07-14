eacdea02378a10496cb1612d38eb4195d74bc543
The Run <TheRun98@users.noreply.github.com>
2017-07-14 17:45:55 -0400

    Add requestroom aliases

--- spacialgaze-plugins/roomrequest.js
+++ spacialgaze-plugins/roomrequest.js
@@ -11,6 +11,8 @@ let allowRequests = true;
 
 exports.commands = {
 	confirmrequestroom: 'requestroom',
+	roomrequest: 'requestroom',
+	rr: 'requestroom',
 	requestroom: function (target, room, user, connection, cmd) {
 		if (!user.named) return this.errorReply(`Please choose a name before requesting a room`);
 		if (!user.registered) return this.errorReply(`Unregistered names cannot be promoted, so they cannot request rooms.`);
