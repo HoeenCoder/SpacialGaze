7364d21115b68baebef30ed196363e569b959475
returningavenger <miguelmayers2001@gmail.com>
2017-09-07 14:58:18 +0000

    Ontime: Add new command /ontimeladder

--- spacialgaze-plugins/ontime.js
+++ spacialgaze-plugins/ontime.js
@@ -22,6 +22,45 @@ function displayTime(t) {
 	return t.h + (t.h === 1 ? " hour " : " hours ") + t.m + (t.m === 1 ? " minute " : " minutes ") + t.s + (t.s === 1 ? " second" : " seconds");
 }
 
+//Shamelessly ripped from economy
+function rankLadder(title, type, array, prop, group) {
+	let groupHeader = group || 'Username';
+	const ladderTitle = '<center><h4><u>' + title + '</u></h4></center>';
+	const thStyle = 'class="rankladder-headers default-td" style="background: -moz-linear-gradient(#576468, #323A3C); background: -webkit-linear-gradient(#576468, #323A3C); background: -o-linear-gradient(#576468, #323A3C); background: linear-gradient(#576468, #323A3C); box-shadow: -1px -1px 2px rgba(0, 0, 0, 0.3) inset, 1px 1px 1px rgba(255, 255, 255, 0.7) inset;"';
+	const tableTop = '<div style="max-height: 310px; overflow-y: scroll;">' +
+		'<table style="width: 100%; border-collapse: collapse;">' +
+		'<tr>' +
+			'<th ' + thStyle + '>Rank</th>' +
+			'<th ' + thStyle + '>' + groupHeader + '</th>' +
+			'<th ' + thStyle + '>' + type + '</th>' +
+		'</tr>';
+	const tableBottom = '</table></div>';
+	const tdStyle = 'class="rankladder-tds default-td" style="box-shadow: -1px -1px 2px rgba(0, 0, 0, 0.3) inset, 1px 1px 1px rgba(255, 255, 255, 0.7) inset;"';
+	const first = 'class="first default-td important" style="box-shadow: -1px -1px 2px rgba(0, 0, 0, 0.3) inset, 1px 1px 1px rgba(255, 255, 255, 0.7) inset;"';
+	const second = 'class="second default-td important" style="box-shadow: -1px -1px 2px rgba(0, 0, 0, 0.3) inset, 1px 1px 1px rgba(255, 255, 255, 0.7) inset;"';
+	const third = 'class="third default-td important" style="box-shadow: -1px -1px 2px rgba(0, 0, 0, 0.3) inset, 1px 1px 1px rgba(255, 255, 255, 0.7) inset;"';
+	let midColumn;
+
+	let tableRows = '';
+
+	for (let i = 0; i < array.length; i++) {
+		if (i === 0) {
+			midColumn = '</td><td ' + first + '>';
+			tableRows += '<tr><td ' + first + '>' + (i + 1) + midColumn + SG.nameColor(array[i].name, true) + midColumn + array[i][prop] + '</td></tr>';
+		} else if (i === 1) {
+			midColumn = '</td><td ' + second + '>';
+			tableRows += '<tr><td ' + second + '>' + (i + 1) + midColumn + SG.nameColor(array[i].name, true) + midColumn + array[i][prop] + '</td></tr>';
+		} else if (i === 2) {
+			midColumn = '</td><td ' + third + '>';
+			tableRows += '<tr><td ' + third + '>' + (i + 1) + midColumn + SG.nameColor(array[i].name, true) + midColumn + array[i][prop] + '</td></tr>';
+		} else {
+			midColumn = '</td><td ' + tdStyle + '>';
+			tableRows += '<tr><td ' + tdStyle + '>' + (i + 1) + midColumn + SG.nameColor(array[i].name, true) + midColumn + array[i][prop] + '</td></tr>';
+		}
+	}
+	return ladderTitle + tableTop + tableRows + tableBottom;
+}
+
 exports.commands = {
 	'!ontime': true,
 	nolife: 'ontime',
@@ -44,5 +83,30 @@ exports.commands = {
 		} else {
 			this.sendReplyBox(SG.nameColor(userid, true) + '\'s total ontime is <b>' + displayTime(convertTime(totalOntime)) + '</b>.' + ' Currently not online.');
 		}
+	},
+	nolifeladder: 'ontimeladder',
+	mostonline: 'ontimeladder',
+	ontimeladder: function (target, room, user) {
+		if (!this.runBroadcast()) return;
+		let keys = Db.ontime.keys().map(name => {
+			let currentOntime = 0;
+			if (Ontime[name]) currentOntime = Date.now() - Ontime[name];
+			const totalOntime = Db.ontime.get(name, 0) + currentOntime;
+			return {
+				name: name,
+				time: totalOntime,
+			};
+		});
+		if (!keys.length) return this.sendReplyBox("Ontime ladder is empty.");
+		keys.sort(function (a, b) {
+			return b.time - a.time;
+		});
+		keys = keys.slice(0, 100).map(function (user) {
+			return {
+				name: user.name,
+				time: displayTime(convertTime(user.time)),
+			};
+		});
+		this.sendReplyBox(rankLadder('Ontime Ladder', 'Total Ontime', keys, 'time'));
 	},
 };
